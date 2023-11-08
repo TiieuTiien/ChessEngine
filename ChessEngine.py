9bd99eb1fb328f626e9911b71f59300bc2f4a482
TiieuTiien <phamquoctien1903@gmail.com>
2023-11-08 18:26:44 +0700

    All possible moves implemented!

--- ChessEngine.py
+++ ChessEngine.py
@@ -7,26 +7,26 @@ responsible for determining the valid moves at the current state. It will also k
 class GameState:
     def __init__(self):
         # Normal board
-        # self.board = [
-        #     ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-        #     ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
-        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
-        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
-        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
-        #     ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-        #     ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
-        # ]
         self.board = [
-            ["bR_Neo","bN_Neo","bB_Neo","bQ_Neo","bK_Neo","bB_Neo","bN_Neo","bR_Neo"],
-            ["bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo"],
-            ["--", "--", "--", "--", "--", "wP_Neo", "--", "--"],
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "bP_Neo", "--", "--", "--"],
-            ["wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo"],
-            ["wR_Neo","wN_Neo","wB_Neo","wQ_Neo","wK_Neo","wB_Neo","wN_Neo","wR_Neo"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        # self.board = [
+        #     ["bR_Neo","bN_Neo","bB_Neo","bQ_Neo","bK_Neo","bB_Neo","bN_Neo","bR_Neo"],
+        #     ["bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo","bP_Neo"],
+        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
+        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
+        #     ["--", "--", "--", "--", "--", "--", "--", "--"],
+        #     ["--", "--", "--", "--", "wR_Neo", "--", "--", "--"],
+        #     ["wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo","wP_Neo"],
+        #     ["wR_Neo","wN_Neo","wB_Neo","wQ_Neo","wK_Neo","wB_Neo","wN_Neo","wR_Neo"],
+        # ]
 
         self.moveFunctions = {
             'P': self.getPawnMoves,
@@ -121,38 +121,95 @@ class GameState:
                     moves.append(Move((r, c), (r+1, c+1), self.board))
 
     '''
-    Get all the rock moves for the pawn located at row, col and add these moves to the list
+    Get all the rock moves for the rock located at row, col and add these moves to the list
     '''
     def getRockMoves(self, r, c, moves):
-        pass
+        # # Check Up
+        # for up in range (r-1, -1, -1):
+        #     if self.board[up][c] == "--":
+        #         moves.append(Move((r, c), (up, c), self.board))
+        # # Check Down
+        # for down in range (r+1, 8):
+        #     if self.board[down][c] == "--":
+        #         moves.append(Move((r, c), (down, c), self.board))
+        # # Check Left
+        # for left in range (c-1, -1, -1):
+        #     if self.board[r][left] == "--":
+        #         moves.append(Move((r, c), (r, left), self.board))
+        # # Check Right
+        # for right in range (r+1, 8):
+        #     if self.board[r][right] == "--":
+        #         moves.append(Move((r, c), (r, right), self.board))
+
+        # Optimize way
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        for dr, dc in directions:
+            new_r, new_c = r + dr, c + dc
+            while 0 <= new_r < 8 and 0 <= new_c < 8:
+                new_pos = self.board[new_r][new_c]
+                if new_pos == "--" or new_pos[0] != self.board[r][c][0]:    
+                    moves.append(Move((r, c), (new_r, new_c), self.board))
+                    if new_pos != "--" and new_pos[0] != self.board[r][c][0]:
+                        break   
+                    new_r += dr
+                    new_c += dc
+                else:
+                    break
 
     '''
-    Get all the rock moves for the pawn located at row, col and add these moves to the list
+    Get all the knight moves for the knight located at row, col and add these moves to the list
     '''
     def getKnightMoves(self, r, c, moves):
-        pass
+        directions = [(-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
+
+        for dr, dc in directions:
+            new_r, new_c = r + dr, c + dc
+            if 0 <= new_r < 8 and 0 <= new_c < 8 and self.board[new_r][new_c][0] != self.board[r][c][0]:
+                moves.append(Move((r, c), (new_r, new_c), self.board))
 
     '''
-    Get all the rock moves for the pawn located at row, col and add these moves to the list
+    Get all the bishop moves for the bishop located at row, col and add these moves to the list
     '''
     def getBishopMoves(self, r, c, moves):
-        pass
+        # Optimize way
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+        for dr, dc in directions:
+            new_r, new_c = r + dr, c + dc
+            while 0 <= new_r < 8 and 0 <= new_c < 8:
+                new_pos = self.board[new_r][new_c]
+                if new_pos == "--" or new_pos[0] != self.board[r][c][0]:
+                    moves.append(Move((r, c), (new_r, new_c), self.board))
+                    if new_pos != "--" and new_pos[0] != self.board[r][c][0]:
+                        break
+                    new_r += dr
+                    new_c += dc
+                else:
+                    break
 
     '''
-    Get all the rock moves for the pawn located at row, col and add these moves to the list
+    Get all the queen moves for the queen located at row, col and add these moves to the list
     '''
     def getQueenMoves(self, r, c, moves):
-        pass
+        self.getBishopMoves(r, c, moves)
+        self.getRockMoves(r, c, moves)
 
     '''
-    Get all the rock moves for the pawn located at row, col and add these moves to the list
+    Get all the king moves for the king located at row, col and add these moves to the list
     '''
     def getKingMoves(self, r, c, moves):
-        pass
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+        for dr, dc in directions:
+            new_r, new_c = r + dr, c + dc
+            if 0 <= new_r < 8 and 0 <= new_c < 8 and self.board[new_r][new_c][0] != self.board[r][c][0]:
+                moves.append(Move((r, c), (new_r, new_c), self.board))
+        
 
 class Move:
     # Represent chess board Ranks and Files (Row and Column in programing context)
-    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, " 8": 0}
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {v: k for k, v in ranksToRows.items()}
 
     filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
@@ -178,9 +235,7 @@ class Move:
 
     def getChessNotation(self):
         # Can be modify to show real chess notation
-        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(
-            self.endRow, self.endCol
-        )
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
 
     def getRankFile(self, r, c):
         return self.colsToFiles[c] + self.rowsToRanks[r]
