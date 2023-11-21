93951137cdd04ef911ce81ff8c5070dd525d0241
TiieuTiien <phamquoctien1903@gmail.com>
2023-11-21 23:31:45 +0700

    Add move log panel

--- ChessMain.py
+++ ChessMain.py
@@ -4,9 +4,11 @@ This  is our main driver file. It will be responsible for handling user input an
 import pygame as p
 import ChessEngine, ChessAI
 
-WIDTH = HEIGHT = 512
+BOARD_WIDTH = BOARD_HEIGHT = 512
+MOVE_LOG_PANEL_WIDTH = 250
+MOVE_LOG_PANEL_HEIGHT = BOARD_HEIGHT
 DIMENTIONS = 8
-SQ_SIZE = HEIGHT // DIMENTIONS
+SQ_SIZE = BOARD_HEIGHT // DIMENTIONS
 MAX_FPS = 16
 IMAGES = {}
 
@@ -28,10 +30,13 @@ def main():
     p.init()
     p.display.set_caption("Chess")
     p.display.set_icon(p.image.load('images/neo/bQ.png'))
-    screen = p.display.set_mode((WIDTH, HEIGHT), p.SRCALPHA)
+    screen = p.display.set_mode((BOARD_WIDTH + MOVE_LOG_PANEL_WIDTH, BOARD_HEIGHT), p.SRCALPHA)
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+
+    # Move log font
+    moveLogFont = p.font.SysFont("Arial", 14, False, False)
     
     # Generate validmoves
     validMoves = gs.getValidMoves()
@@ -49,7 +54,7 @@ def main():
 
     gameOver = False
     playerOne = True # If human playing white then player one is true and vice versa
-    playerTwo = False # Same as above
+    playerTwo = True # Same as above
 
     while running:
         humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
@@ -61,7 +66,8 @@ def main():
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
-                    if sqSelected == (row, col):
+                    # The user clicked the mouse twice or user clicked mouse log
+                    if sqSelected == (row, col) or col >= 8:
                         sqSelected = ()
                         playerClicks = []
                     else:
@@ -106,20 +112,51 @@ def main():
             validMoves = gs.getValidMoves()
             moveMade = False
 
-        drawGameState(screen, gs, validMoves, sqSelected)
+        drawGameState(screen, gs, validMoves, sqSelected, moveLogFont)
 
-        if gs.checkMate:
+        if gs.checkMate or gs.staleMate:
             gameOver = True
-            if gs.whiteToMove:
-                drawText(screen, 'Black wins by checkmate')
-            elif not gs.whiteToMove:
-                drawText(screen, 'White wins by checkmate')
-        elif gs.staleMate:
-            gameOver = True
-            drawText(screen, 'Stale mate!!!')
+            text = 'Stale mate!!!' if gs.staleMate else 'Black wins by checkmate' if gs.whiteToMove else 'White wins by checkmate'
+            drawEndGameText(screen, text)
 
         clock.tick(MAX_FPS)
         p.display.flip()
+
+'''
+Responsible for all the graphic within a current game state
+'''
+def drawGameState(screen, gs, validMoves, sqSelected, moveLogFont):
+    drawBoard(screen)
+    highlightSquares(screen, gs, validMoves, sqSelected)
+    drawPieces(screen, gs.board)
+    drawMoveLog(screen, gs, moveLogFont)
+
+'''
+Draw the square
+'''
+def drawBoard(screen):
+    WHITE = (237, 238, 209)
+    GREEN = (119, 153, 82)
+    colors = [p.Color(WHITE), p.Color(GREEN)]
+
+    for r in range(DIMENTIONS):
+        for c in range(DIMENTIONS):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    
+    # Display Files and Ranks:
+        # Display numbers only on the "a" file
+        font = p.font.SysFont('comicsans', 14, bold=True)
+        text = font.render(str(8 - r), True, colors[0] if r % 2 == 1 else colors[1])
+        text_rect = text.get_rect(center=(SQ_SIZE // 8, r * SQ_SIZE + SQ_SIZE // 4))
+        screen.blit(text, text_rect)
+        
+    # Display file letters on the top
+    for c in range(DIMENTIONS):
+        font = p.font.SysFont('comicsans', 18)
+        text = font.render(chr(97 + c), True, colors[0] if c % 2 == 0 else colors[1])
+        text_rect = text.get_rect(bottomright=(c * SQ_SIZE + SQ_SIZE - SQ_SIZE / 32, 8 * SQ_SIZE))
+        screen.blit(text, text_rect)
 
 '''
 Hightlight square selected and moves for piece selected
@@ -153,41 +190,6 @@ def drawDot(screen, center, alpha):
     p.draw.circle(screen, inner_color, center, inner_radius)
 
 '''
-Responsible for all the graphic within a current game state
-'''
-def drawGameState(screen, gs, validMoves, sqSelected):
-    drawBoard(screen)
-    highlightSquares(screen, gs, validMoves, sqSelected)
-    drawPieces(screen, gs.board)
-
-'''
-Draw the square
-'''
-def drawBoard(screen):
-    WHITE = (237, 238, 209)
-    GREEN = (119, 153, 82)
-    colors = [p.Color(WHITE), p.Color(GREEN)]
-
-    for r in range(DIMENTIONS):
-        for c in range(DIMENTIONS):
-            color = colors[((r+c)%2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-    
-    # Display Files and Ranks:
-        # Display numbers only on the "a" file
-        font = p.font.SysFont('comicsans', 14, bold=True)
-        text = font.render(str(8 - r), True, colors[0] if r % 2 == 1 else colors[1])
-        text_rect = text.get_rect(center=(SQ_SIZE // 8, r * SQ_SIZE + SQ_SIZE // 4))
-        screen.blit(text, text_rect)
-        
-    # Display file letters on the top
-    for c in range(DIMENTIONS):
-        font = p.font.SysFont('comicsans', 18)
-        text = font.render(chr(97 + c), True, colors[0] if c % 2 == 0 else colors[1])
-        text_rect = text.get_rect(bottomright=(c * SQ_SIZE + SQ_SIZE - SQ_SIZE / 32, 8 * SQ_SIZE))
-        screen.blit(text, text_rect)
-
-'''
 Draw the pieces
 '''
 def drawPieces(screen, board):
@@ -196,11 +198,40 @@ def drawPieces(screen, board):
             piece = board[r][c]
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+'''
+Draw move log
+'''
+def drawMoveLog(screen, gs, font):
+    moveLogRect= p.Rect(BOARD_WIDTH, 0, MOVE_LOG_PANEL_WIDTH, MOVE_LOG_PANEL_HEIGHT)
+    p.draw.rect(screen, p.Color(50, 50, 50), moveLogRect)
+    moveLog = gs.moveLog
+    moveTexts = []
+    for i in range(0, len(moveLog), 2):
+        moveString = str(i//2 + 1) + ". " + str(moveLog[i]) + "   "
+        # Make sure black make a move
+        if i+1 < len(moveLog):
+            moveString += str(moveLog[i+1]) + "     "
+        moveTexts.append(moveString)
+
+    movePerRow = 3
+    padding = 5
+    lineSpacing = 2
+    textY = padding
+    for i in range(0, len(moveTexts), movePerRow):
+        text = ""
+        for j in range(movePerRow):
+            if i+j < len(moveTexts):
+                text += moveTexts[i+j]
+        textObject = font.render(text, True, p.Color('white'))
+        textLocation = moveLogRect.move(padding, textY)
+        screen.blit(textObject, textLocation)
+        textY += textObject.get_height() + lineSpacing
     
-def drawText(screen, text):
-    font = p.font.SysFont("Gotham", 32, True, False)
-    textObject = font.render(text, 0, p.Color('black'))
-    textLocation = p.Rect(0, 0, WIDTH, HEIGHT).move(WIDTH/2 - textObject.get_width()/2, HEIGHT/2 - textObject.get_height()/2)
+def drawEndGameText(screen, text):
+    font = p.font.SysFont("Montserrat Black", 32, True, False)
+    textObject = font.render(text, 0, p.Color('gray'))
+    textLocation = p.Rect(0, 0, BOARD_WIDTH, BOARD_HEIGHT).move(BOARD_WIDTH/2 - textObject.get_width()/2, BOARD_HEIGHT/2 - textObject.get_height()/2)
     screen.blit(textObject, textLocation)
 
 if __name__ == "__main__":
