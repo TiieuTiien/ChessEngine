d609885197e4cd031a9ccdb0f0023213b6a25ba5
TiieuTiien <phamquoctien1903@gmail.com>
2023-11-21 19:34:57 +0700

    Implement Minimax, Negamax and Alpha-beta prunting

--- ChessAI.py
+++ ChessAI.py
@@ -10,6 +10,7 @@ pieceScore = {
 }
 CHECKMATE = 1000
 STALEMATE = 0
+DEPTH = 3
 
 '''
 Picks and returns a random move.
@@ -18,9 +19,9 @@ def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
 
 '''
-Find the best move based on material alone. Minimax algorithms withou recursion
+Find the best move based on material alone. Minimax algorithms without recursion
 '''
-def findBestMove(gs,validMoves):
+def findBestMoveThings(gs,validMoves):
     turnMultiplier = 1 if gs.whiteToMove else -1
 
     opponentMinMaxScore = CHECKMATE
@@ -29,23 +30,155 @@ def findBestMove(gs,validMoves):
     for playerMove in validMoves:
         gs.makeMove(playerMove)
         opponentsMoves = gs.getValidMoves()
-        opponentMaxScore = -CHECKMATE
-        for opponentsMove in opponentsMoves:
-            gs.makeMove(opponentsMove)
-            if gs.checkMate:
-                score = -turnMultiplier * CHECKMATE
-            elif gs.staleMate:
-                score = STALEMATE
-            else:
-                score = -turnMultiplier * scoreMaterial(gs.board)
-            if(score > opponentMaxScore):
-                opponentMaxScore = score
-            gs.undoMove()
+        if gs.staleMate:
+            opponentMaxScore = STALEMATE
+        elif gs.checkMate:
+            opponentMaxScore = -CHECKMATE
+        else:
+            opponentMaxScore = -CHECKMATE
+            for opponentsMove in opponentsMoves:
+                gs.makeMove(opponentsMove)
+                gs.getValidMoves()
+                if gs.checkMate:
+                    score = CHECKMATE
+                elif gs.staleMate:
+                    score = STALEMATE
+                else:
+                    score = -turnMultiplier * scoreMaterial(gs.board)
+                if(score > opponentMaxScore):
+                    opponentMaxScore = score
+                gs.undoMove()
+
         if opponentMaxScore < opponentMinMaxScore:
             opponentMinMaxScore = opponentMaxScore
             bestPlayerMove = playerMove
         gs.undoMove()
     return bestPlayerMove
+
+'''
+Helper method to make first recursive call
+'''
+def findBestMove(gs, validMoves):
+    global nextMove, counter
+    nextMove = None
+    random.shuffle(validMoves)
+    counter = 0
+    # findMoveMinMax(gs, validMoves, DEPTH, gs.whiteToMove)
+    # findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else -1)
+    findMoveNegaMaxAlphaBeta(gs, validMoves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
+    print(counter)
+    return nextMove
+
+'''
+
+'''
+def findMoveMinMax(gs, validMoves, depth, whiteToMove):
+    global nextMove
+    if depth == 0:
+        return scoreMaterial(gs.board)
+    
+    if whiteToMove:
+        maxScore = -CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = findMoveMinMax(gs, nextMoves, depth - 1, False)
+            if score > maxScore:
+                maxScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return maxScore
+
+    else:
+        minScore = CHECKMATE
+        for move in validMoves:
+            gs.makeMove(move)
+            nextMoves = gs.getValidMoves()
+            score = findMoveMinMax(gs, nextMoves, depth - 1, True)
+            if score < minScore:
+                minScore = score
+                if depth == DEPTH:
+                    nextMove = move
+            gs.undoMove()
+        return minScore
+
+'''
+Find move nega max
+'''
+def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
+    global nextMove
+    # , counter
+    # counter+=1
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveNegaMax(gs, nextMoves, depth-1, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+    return maxScore
+
+'''
+Find move nega max with alpha-beta
+Alpha is for the maximum possible
+Beta is minimum
+'''
+def findMoveNegaMaxAlphaBeta(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    global nextMove
+    # , counter
+    # counter+=1
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    
+    #move ordering - implement later
+
+    
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveNegaMaxAlphaBeta(gs, nextMoves, depth-1, -beta, -alpha, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+        
+        if maxScore > alpha: # Prunting happend
+            alpha = maxScore
+        
+        if alpha >= beta:
+            break
+    return maxScore
+
+'''
+A positive score is good for white, a negative score is good for black
+'''
+def scoreBoard(gs):
+    if gs.checkMate:
+        if gs.whiteToMove:
+            return -CHECKMATE #black wins
+        else:
+            return CHECKMATE #white wins
+    elif gs.staleMate:
+        return STALEMATE
+
+    score = 0
+    for row in gs.board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+
+    return score
 
 '''
 Score the board based on material.
